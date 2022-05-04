56c2a68b7fe8da1ee61b209dc3e4d02db690c5bf
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-05-04 16:26:33 -0300

    feat: add border radius to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
           500: '#8257E6',
         },
       },
+      borderRadius: {
+        md: '4px',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms'), require('tailwind-scrollbar')],
