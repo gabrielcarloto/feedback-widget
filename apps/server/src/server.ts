8b7261366eadeb6804171e4868a25e466b02efea
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2022-05-06 15:46:45 -0300

    feat: send email with nodemailer and mailtrap

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '0aa170acf87a9c',
+    pass: '2f9decc92d24e6',
+  },
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Gabriel Carloto <gabrielcidralcarloto@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #222;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>',
+    ].join('\n'),
   });
 
   return res.status(201).json({ data: feedback });
